d233040956172b99a2e04953176bda76653d5418
Mikxus <91991837+Mikxus@users.noreply.github.com>
2025-06-02 21:55:46 +0300

    simple shell up

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include <stdbool.h>
 
 #include <zephyr/kernel.h>
-#include <zephry/logging/log.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(canbus_shell, LOG_LEVEL_INF);
 
 int main(void)
 {
     while(true)
     {
-        LOG_WARN("TEST");
+        LOG_WRN("TEST");
         k_sleep(K_MSEC(1000));
     }
 }
